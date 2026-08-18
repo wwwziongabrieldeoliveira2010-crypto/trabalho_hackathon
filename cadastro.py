329fc5f56acf087bf43193b9cf92b607137b68a6
Zion Gabriel <www.ziongabrieldeoliveira2010@gmail.com>
2026-08-18 14:57:16 -0300

    novo

--- cadastro.py
+++ cadastro.py
@@ -1,59 +1,27 @@
-from utilitario import *
+from utilitario import conectar
 
-def cadastro_familia():
 
-    while True:
-        sobrenome = input("Digite o sobrenome de sua família: ")
+def cadastro_usuario():
 
-        if vali_txt(sobrenome):
-            break
+    nome = input("Digite o nome do usuário: ")
 
-        else:
-            print("Error o campo deve estar campo não pode estar vazio")
-
-    while True:
-        residentes = input("Digite o número de pessoas em sua casa: ")
-
-        if validar_inteiro(residentes):
-            residentes = int(residentes)
-            break
-
-        else: 
-            print("Erro: digite um número inteiro válido.")
-
-    while True:
-        endereco = input("Digite o endereço de sua fazenda: ")
-
-        if endereco.strip() != "":
-            break
-
-        else:
-         print("Erro: o endereço não pode estar vazio.")
+    
 
     conexao = conectar()
     cursor = conexao.cursor()
-    query = """INSERT INTO familia (sobrenome,  quantidade_de_membros, endereco) VALUES (%s,%s,%s)"""
 
-    try:
+    sql = """
+    INSERT INTO usuario (nome)
+    VALUES (%s)
+    """
 
-        cursor.execute(
-            query,
-            (
-                sobrenome,
-                int(float(residentes)),
-                endereco
-            )
-        )
-        conexao.commit()
-        cursor.close()
-        conexao.close()
-    except mysql.connector.Error as err:
-        print(f"Erro: {err}")
+    valores = (nome)
 
+    cursor.execute(sql, valores)
 
-    print("\n===== CADASTRO =====")
-    print(f"Sobrenome: {sobrenome}")
-    print(f"Residentes: {residentes}")
-    print(f"Endereço: {endereco}")
+    conexao.commit()
 
-cadastro_familia()
+    print("Usuário cadastrado com sucesso!")
+
+    cursor.close()
+    conexao.close()
