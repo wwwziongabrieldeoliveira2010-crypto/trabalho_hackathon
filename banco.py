d12497a57fdeaff4005953f578d46ab73465a5c3
Zion Gabriel <www.ziongabrieldeoliveira2010@gmail.com>
2026-08-18 15:24:10 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -34,47 +34,52 @@ CREATE TABLE IF NOT EXISTS usuario (
 """)
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS residencia (
-    id_residencia INT AUTO_INCREMENT PRIMARY KEY,
-    fk_id_usuario int,
-    proprietario VARCHAR(255),
-    quantidade_de_residentes INT,
-    endereco VARCHAR(255),
-    FOREIGN KEY (fk_id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS agua (
+    id_agua INT AUTO_INCREMENT PRIMARY KEY,
+
+    fk_id_residencia INT NOT NULL,
+
+    litros_de_agua_usados DECIMAL(10,2) NOT NULL,
+
+    preco_por_litro DECIMAL(10,2),
+
+    FOREIGN KEY (fk_id_residencia)
+    REFERENCES residencia(id_residencia)
+    ON DELETE CASCADE
 )
 """)
-
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS energia (
-    id_residencia INT,
-    quilowatts_por_hora DECIMAL(10, 2),
-    preco_do_quilowatt_por_hora DECIMAL(10, 2),
-    FOREIGN KEY (id_residencia)
+    id_energia INT AUTO_INCREMENT PRIMARY KEY,
+
+    fk_id_residencia INT NOT NULL,
+
+    quilowatts_por_hora DECIMAL(10,2) NOT NULL,
+
+    preco_do_quilowatt_por_hora DECIMAL(10,2),
+
+    FOREIGN KEY (fk_id_residencia)
     REFERENCES residencia(id_residencia)
     ON DELETE CASCADE
 )
 """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS energia (
-    id_residencia INT,
-    litros_de_agua_usados DECIMAL(10, 2),
-    preco_por_litro DECIMAL(10, 2),
-    FOREIGN KEY (id_residencia)
-    REFERENCES residencia(id_residencia)
-    ON DELETE CASCADE
-)
-""")
 
-
-
+# ==========================================
+# TABELA LIXO
+# ==========================================
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS lixo (
-    id_residencia INT,
-    quilo_de_lixo DECIMAL(10, 2),
-    multa_por_quilo_de_lixo DECIMAL(10, 2),
-    FOREIGN KEY (id_residencia)
+    id_lixo INT AUTO_INCREMENT PRIMARY KEY,
+
+    fk_id_residencia INT NOT NULL,
+
+    quilo_de_lixo DECIMAL(10,2) NOT NULL,
+
+    multa_por_quilo_de_lixo DECIMAL(10,2),
+
+    FOREIGN KEY (fk_id_residencia)
     REFERENCES residencia(id_residencia)
     ON DELETE CASCADE
 )
