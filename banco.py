ee60936bee356e91c528a63901538fce71c99f38
PedroFelipeLima <pedrofelipeferreiralimaaaaaa@gmail.com>
2026-08-18 11:30:38 -0300

    banco de dados

--- banco.py
+++ banco.py
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-import mysql.connector 
-
-
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="18062010"
-)
-
-cursor = db.cursor()
-
-
-
-cursor.execute("CREATE DATABASE IF NOT EXISTS banco")
-=======
 import mysql.connector
 from datetime import datetime
 
 db = mysql.connector.connect(
-    host="locahost",
+    host="localhost",
     user="root",
-    password="18062010"
+    password="01t0M31@"
     )
 
 cursor = db.cursor()
@@ -29,13 +13,40 @@ cursor.execute("CREATE DATABASE IF NOT EXISTS fazendeiro")
 cursor.execute("USE fazendeiro")
 
 cursor.execute("""create table if not exists graos (
- id_grao int primary key,
- tipo varchar (255));""")
+ id_grao int AUTO_INCREMENT primary key,
+ tipo varchar (255) UNIQUE NOT NULL);""")
 
-cursor.execute("""create table if not exists movimento (
- id_movimentacao int,
- id_grao int,
- gramas decimal (10, 2),
- uso  varchar(255),
- FOREIGN KEY (id_grao) REFERENCES graos(id_graos) ON DELETE CASCADE);""")
->>>>>>> c2ecf7131a77409b20d2b936456dd3f9d009362a
+tipo = [
+    ("Arroz",),
+    ("Feijão",),
+    ("Milho",),
+    ("Trigo",),
+    ("Soja",)
+]
+cursor.executemany("""INSERT IGNORE INTO graos (tipo) VALUES (%s)""", tipo)
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS estoque (
+ id_estoque int primary key,
+ fk_id_grao int,
+ quantidade float,
+ FOREIGN KEY (fk_id_grao) REFERENCES graos(id_grao) ON DELETE CASCADE
+ );""")
+
+cursor.execute("""create table if not exists logs (
+ id_log int,
+ fk_id_grao int,
+ quantidade float,
+ acao varchar(255),
+ data_hora DATETIME,
+ FOREIGN KEY (fk_id_grao) REFERENCES graos(id_grao) ON DELETE CASCADE
+ );""")
+
+cursor.execute("""create table if not exists logs (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id_graos int,
+  usuario varchar(255),
+  gramas decimal(10, 2),
+  uso varchar(255),
+  data datetime,
+  FOREIGN KEY (id_grao) REFERENCES graos(id_grao) ON DELETE CASCADE
+);""")
