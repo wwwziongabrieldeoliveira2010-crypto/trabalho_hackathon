81242579534a393fdc98e8de6173584a26e93c48
Zion Gabriel <www.ziongabrieldeoliveira2010@gmail.com>
2026-08-19 08:44:26 -0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,12 @@
+def menu():
+    while True:
+
+        print("\n================================")
+        print(" SISTEMA DE SUSTENTABILIDADE")
+        print("================================")
+        print("1 - Cadastrar residência")
+        print("2 - Cadastrar gastos de água")
+        print("3 - Cadastrar gastos de energia")
+        print("4 - Cadastrar produção de lixo")
+        print("5 - Atualizar gastos")
+menu()
